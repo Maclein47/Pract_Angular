59cdd36073d3109598b4c3062faf7ec1b2efd2c2
Angel Majan <agmajan@hotmail.com>
2023-09-21 13:47:02 +0200

    210923 13:46 - con compnt zapatillas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videoJuego/videojuego.component';
-<<<<<<< Updated upstream
-import { ZapatillasComponent } from './zapatillas/zapatillas';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
-=======
-import {ZapatillasComponent} from './zapatillas/zapatillas.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
